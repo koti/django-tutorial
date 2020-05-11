76a2b8fc85ed6123129fccc9a3b3042de43be828
Menelaos <menelaoskotoglou@gmail.com>
2020-05-12 01:18:02 +0300

    second commit

--- django_tutorial/django_tutorial/urls.py
+++ django_tutorial/django_tutorial/urls.py
@@ -1,7 +1,6 @@
-"""django_tutorial URL Configuration
-
+"""pollster URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,10 +14,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("polls/", include("polls.urls", namespace="polls")),
+    path("", include("pages.urls")),
+    path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
-    path("", RedirectView.as_view(url="/polls/")),
 ]
